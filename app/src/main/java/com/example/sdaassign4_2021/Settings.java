28cf0029630f2bda39e819f19904cd3dd4bfdad5
Paul Fennell <paul.fennell3@mail.dcu.ie>
2022-01-31 17:08:40 +0000

    Created the shared preferences for saving detail and i have add a checkbox to save users details

--- app/src/main/java/com/example/sdaassign4_2021/Settings.java
+++ app/src/main/java/com/example/sdaassign4_2021/Settings.java
@@ -24,6 +24,11 @@ import android.widget.Toast;
  * A simple {@link Fragment} subclass.
  */
 public class Settings extends Fragment {
+    EditText userId, userEmail, userBorId;
+    Button saveDet,resetData;
+    CheckBox checkBox;
+    String mUser,mEmail,mBorId;
+    SharedPreferences sp;
 
     public Settings() {
         // Required empty public constructor
@@ -34,6 +39,52 @@ public class Settings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        userId = view.findViewById(R.id.userName);
+        userEmail = view.findViewById(R.id.email);
+        userBorId = view.findViewById(R.id.borrowerID);
+        saveDet = view.findViewById(R.id.button);
+        resetData = view.findViewById(R.id.button2);
+        checkBox = view.findViewById(R.id.checkBox);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences.Editor editor = prefs.edit();
+
+        String st1 = prefs.getString("I","");
+        String st2 = prefs.getString("E","");
+        String st3= prefs.getString("B","");
+
+        userId.setText(st1);
+        userEmail.setText(st2);
+        userBorId.setText(st3);
+
+        saveDet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if(verifyEmailAddress() == true){
+                    if(checkBox.isChecked()) {
+                        //Save the UserId
+                        mUser = userId.getText().toString();
+                        mEmail = userEmail.getText().toString();
+                        mBorId = userBorId.getText().toString();
+                        editor.putString("I", mUser);
+                        editor.putString("E", mEmail);
+                        editor.putString("B", mBorId);
+                        editor.commit();
+                        Log.d("Test 1", "onClick: ");
+                        Toast.makeText(getContext(), "Info saved", Toast.LENGTH_SHORT).show();
+                    }else{
+                        editor.putString("I", "");
+                        editor.putString("E", "");
+                        editor.putString("B", "");
+                        Log.d("Test 2", "onClick: ");
+                        Toast.makeText(getContext(), "Information will not be saved ", Toast.LENGTH_SHORT).show();
+                    }
+                }//else {
+                  //  verifyEmailAddress();
+               // }
+           // }
+        });
 
         return view;
     }

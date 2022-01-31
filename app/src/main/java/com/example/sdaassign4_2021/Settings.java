b5e4e28a43aaf3184694a0e81343432195323c44
Paul Fennell <paul.fennell3@mail.dcu.ie>
2022-01-31 17:04:24 +0000

    My Initial commit for assignment 4

---
+++ app/src/main/java/com/example/sdaassign4_2021/Settings.java
@@ -0,0 +1,41 @@
+package com.example.sdaassign4_2021;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Settings extends Fragment {
+
+    public Settings() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return view;
+    }
+
+}
